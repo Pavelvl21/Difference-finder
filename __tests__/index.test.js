97b9175e71d50bf85152f5720328a0e2524d8a6a
Pavel <p.yudenko@gmail.com>
2022-08-06 22:59:34 +0300

    fix eslint errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,9 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { readFileSync } from 'fs';
+import genDiff from '../src/index.js';
 
-
-
-import genDiff from '../src/index.js'
-
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const file1 = getFixturePath('file1.json');
