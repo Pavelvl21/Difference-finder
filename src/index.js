b99cdee4db15df3449523d37c8dd5dd6dc0f5c0d
Pavel <p.yudenko@gmail.com>
2022-08-22 22:26:01 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import path from 'path';
 import _ from 'lodash';
 import parseData from './parsers.js';
+import treeBuilder from './treeBuilder.js'
+import diffBuilder from './stylish.js';
+
+
 
 const getPath = (filepath) => path.resolve(process.cwd(), filepath);
 
@@ -10,22 +14,8 @@ const genDiff = (filepath1, filepath2) => {
 
   const data1 = parseData(pathToFile1);
   const data2 = parseData(pathToFile2);
-
-  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
-
-  const result = keys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      return `  - ${key}: ${value1}`;
-    }
-    if (!_.has(data1, key) && _.has(data2, key)) {
-      return `  + ${key}: ${value2}`;
-    }
-    return value1 === value2 ? `    ${key}: ${value2}` : `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
-  });
-
-  return (`{\n${result.join('\n')}\n}`);
+  
+  return diffBuilder(treeBuilder(data1, data2));
 };
 
 export default genDiff;
