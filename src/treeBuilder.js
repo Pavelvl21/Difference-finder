2934d998dd869d75673b7f2eef7e550d45d0035b
Pavel <p.yudenko@gmail.com>
2022-08-22 22:25:28 +0300

    add treeBuilder.js

---
+++ src/treeBuilder.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+
+const treeBuilder = (tree1, tree2) => {
+  return _.sortBy(_.union(_.keys(tree1), _.keys(tree2)))
+    .map((key) => {
+      const value1 = tree1[key];
+      const value2 = tree2[key];
+
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return {
+        children: treeBuilder(value1, value2),
+        key,
+        spaceMark: '  ',
+        status: 'nested',
+      };
+    }
+    if (_.has(tree1, key) && !_.has(tree2, key)) {
+      return {
+        diffMark: '- ',
+        key,
+        status: 'deleted',        
+        value: value1,        
+      };
+    }
+    if (!_.has(tree1, key) && _.has(tree2, key)) {
+      return {
+        diffMark: '+ ',
+        key,
+        status: 'added',
+        value: value2,        
+      };
+    }
+    if (value1 !== value2) {
+      return {
+        addDiffMark: '+ ',
+        key,
+        status: 'changed',
+        remDiffMark: '- ',
+        value1,
+        value2,
+      };
+    }
+    return {
+      key,
+      spaceMark: '  ',
+      status: 'unchanged',
+      value: value1,
+    };
+  });
+};
+
+export default treeBuilder;
