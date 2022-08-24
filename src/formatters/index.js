224419b7d44620c90f540af34f20611818c1dbb5
Pavel <p.yudenko@gmail.com>
2022-08-24 21:02:41 +0300

    refactoring

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,14 +1,13 @@
 import stylishFormatter from './stylish.js';
+import plainFormatter from './plain.js';
 
 const diffFormatter = (tree, formatter = 'stylish') => {
   switch (formatter) {
     case 'stylish':
       return stylishFormatter(tree);
-    default: try {
-      throw new Error(`Formatter '${formatter}' is not supported`);
-    } catch (error) {
-      return error.message;
-    }
+    case 'plain':
+      return plainFormatter(tree);
+    default: throw new Error(`Formatter '${formatter}' is not supported`);
   }
 };
 
